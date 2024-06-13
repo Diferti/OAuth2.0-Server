66ea609ed452b10bdb8a10d7ca56111e0518dfbd
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2024-06-12 22:28:52 -0400

    Added Client Credentials Grant method

--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 dataBase =  {
     clients: [
-        { id: 1, clientId: 'client1', clientSecret: 'secret1' },
-        { id: 2, clientId: 'client2', clientSecret: 'secret2' }
+        { idClient: 'client1', clientSecret: 'secret1' },
+        { idClient: 'client2', clientSecret: 'secret2' }
     ],
     users: [
-        { id: 1, username: 'user1', password: 'pass1' },
-        { id: 2, username: 'user2', password: 'pass2' }
+        { username: 'user1', password: 'pass1' },
+        { username: 'user2', password: 'pass2' }
     ],
     tokens: []
 };
+
+module.exports = {dataBase};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const parser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const data = require('../src/data.js');
+const {dataBase} = require('../src/data.js');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8000;
 
 const server = express();
+server.use(cors());
+server.use(parser.json());
+server.use(parser.urlencoded({ extended: true }));
+
+server.post('/clientCredentials', (req, res) => {
+    const {idClient, clientSecret, grantType} = req.body;
+
+    if (grantType === 'clientCredentials')
+    {
+        let client = dataBase.clients.find((x) => x.idClient === idClient && x.clientSecret === clientSecret);
+        if (client)
+        {
+            return res.status(200).json({ access_token: 'client_credentials_access_token' })
+        }
+    }
+    res.status(400).json({error: 'Invalid data'});
+});
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
